f12c6d329e583f3222db6b464a5ad596419130df
max.nelson <max.nelson@dish.com>
2023-10-03 14:19:36 -0600

    Added card ui

--- app/src/main/java/com/example/composetest/ui/AddWorkoutActivity.kt
+++ app/src/main/java/com/example/composetest/ui/AddWorkoutActivity.kt
@@ -7,6 +7,7 @@ import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -37,6 +39,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedCard
@@ -49,12 +52,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,6 +68,7 @@ import com.example.composetest.R
 import com.example.composetest.model.Exercise
 import com.example.composetest.ui.theme.NavyBlue
 import com.example.composetest.ui.theme.NeonOrange
+import com.example.composetest.ui.theme.SecondaryBlue
 import com.example.composetest.ui.theme.Typography
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -96,7 +102,6 @@ class AddWorkoutActivity: ComponentActivity() {
         var showPopup by remember { mutableStateOf(false) }
         val _exerciseList = remember { MutableStateFlow(listOf<Exercise>()) }
         val exerciseList by remember { _exerciseList }.collectAsState()
-
 
         if (showPopup) {
 
@@ -300,14 +305,22 @@ class AddWorkoutActivity: ComponentActivity() {
                 }
             }
         ) {
-            Box(modifier = Modifier
-                .background(NavyBlue)
-                .padding(it)
-                .fillMaxSize()
+            Column(
+                modifier = Modifier
+                    .background(NavyBlue)
+                    .padding(it)
+                    .fillMaxSize()
             ) {
-                Column {
-                    Spacer(modifier = Modifier.height(24.dp))
-                    TopBar()
+                Spacer(modifier = Modifier.height(24.dp))
+                TopBar()
+                if (exerciseList.isEmpty()) {
+                    Text(
+                        text = "No Exercises Added Yet",
+                        style = Typography.h6,
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .padding(start = 16.dp, top = 24.dp))
+                } else {
                     ExerciseList(exercises = exerciseList)
                 }
             }
@@ -404,26 +417,89 @@ class AddWorkoutActivity: ComponentActivity() {
     @Composable
     fun ExerciseList(exercises: List<Exercise>) {
         LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.fillMaxWidth(),
             contentPadding = PaddingValues(16.dp)
         ) {
-            if (exercises.isEmpty()) {
-                item {
-                    Text(
-                        text = "No Exercises Added Yet",
-                        style = Typography.h5)
-                }
-            } else {
-                items(exercises) {exercise ->
-                    ExerciseCard(exercise = exercise)
-                }
+            items(exercises) {exercise ->
+                ExerciseCard(exercise = exercise)
             }
         }
     }
 
     @Composable
     fun ExerciseCard(exercise: Exercise) {
-        Box() {
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(SecondaryBlue)
+                .padding(10.dp)
+                .fillMaxWidth(),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = exercise.name,
+                    style = Typography.h5,
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 16.sp)
+                Icon(
+                    imageVector = Icons.Outlined.Edit,
+                    contentDescription = "Edit Button",
+                    tint = Color.White,
+                    modifier = Modifier.padding(end = 8.dp))
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+            ) {
+                Text(
+                    text = exercise.shotsMade.toString(),
+                    style = Typography.h5,
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 14.sp)
+                Text(
+                    text = "/" + exercise.totalShots.toString(),
+                    style = Typography.h5,
+                    fontSize = 14.sp)
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 6.dp)
+            ) {
+                Text(
+                    text = "Range: ",
+                    style = Typography.h5,
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 14.sp)
+
+                Text(
+                    text = exercise.range,
+                    style = Typography.h5,
+                    fontSize = 14.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 6.dp)
+            ) {
+                Text(
+                    text = "Angle: ",
+                    style = Typography.h5,
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 14.sp
+                )
+                Text(
+                    text = exercise.location,
+                    style = Typography.h5,
+                    fontSize = 14.sp
+                )
+            }
 
         }
     }
